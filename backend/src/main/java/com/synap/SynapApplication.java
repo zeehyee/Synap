5fcb4e945327a3f51615d0a3a74875885a6e3b3a
zeehyee <amber9324@naver.com>
2026-04-01 19:23:51 +0200

    feat: 초기 프로젝트 환경설정

---
+++ backend/src/main/java/com/synap/SynapApplication.java
@@ -0,0 +1,13 @@
+package com.synap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/synap/SynapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
